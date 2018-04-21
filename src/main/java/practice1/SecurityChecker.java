e0a6c4d871b590263332c8f8a3756932d1379c86
AScodersWH <895052253@qq.com>
2018-04-21 23:52:09 +0800

    [master] refactor

---
+++ src/main/java/practice1/SecurityChecker.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class SecurityChecker {
+    public  boolean checkPermission(User user, Permission permission) {
+        boolean flag = true;
+        if(user == null || permission == null ) flag = false;
+        return flag;
+    }
+
+    public boolean isAdmin() {
+        //todo code
+        return false;
+    }
+}
